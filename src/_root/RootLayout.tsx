fb23a86e810097a1dc860cc1a66202c4ee42f5f7
V Adarsh <adarshmanjady@gmail.com>
2023-12-16 18:15:20 +0530

    added footer

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,22 +1,20 @@
-import Footer from '@/components/shared/Footer';
-import LeftSideBar from '@/components/shared/LeftSideBar';
-import Navbar from '@/components/shared/Navbar';
+import Footer from "@/components/shared/Footer";
+import LeftSideBar from "@/components/shared/LeftSideBar";
+import Navbar from "@/components/shared/Navbar";
 
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   return (
-    <div className='w-full md:flex bg-black text-white'>
+    <div className="w-full md:flex flex-1  text-white h-screen">
       <Navbar />
       <LeftSideBar />
-
-      <section className='flex flex-1 h-full'>
+      <section className="flex flex-1 h-screen ">
         <Outlet />
       </section>
-
       <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default RootLayout;
--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { NavLink, useLocation } from 'react-router-dom'
+import { HiMiniHome } from "react-icons/hi2";
+import { MdExplore,MdCreateNewFolder } from "react-icons/md";
+
+import { BsPeopleFill,BsSaveFill } from "react-icons/bs";
+
 
 const Footer = () => {
+  const {pathname} = useLocation()
   return (
-    <div>Footer</div>
+    <div className='sticky bottom-0 md:hidden '>
+      <ul className="flex  gap-8 h-full">
+          <li className={`flex flex-col gap-2 text-xs sm:text-sm w-full items-center hover:bg-violet-300 hover:text-gray-700 font-semibold rounded-[10px] py-2 text-center ${pathname === '/' && 'bg-violet-300 text-gray-800'} `}>
+            <HiMiniHome />
+            <NavLink className='' to="/">Home</NavLink>
+          </li>
+          <li className={`flex flex-col gap-2 text-xs sm:text-sm w-full items-center hover:bg-violet-300 hover:text-gray-700 font-semibold rounded-[10px] py-2 text-center ${pathname === '/create-post' && 'bg-violet-300 text-gray-800'} `}>
+          
+          <MdCreateNewFolder />
+            <NavLink className='' to="/create-post">Create</NavLink>
+          </li>
+          <li className={`flex flex-col gap-2 text-xs sm:text-sm w-full items-center hover:bg-violet-300 hover:text-gray-700 font-semibold rounded-[10px] py-2 text-center ${pathname === '/explore' && 'bg-violet-300 text-gray-800'} `}>
+          <MdExplore />
+            <NavLink className='' to="/explore">Explore</NavLink>
+          </li>
+          <li className={`flex flex-col gap-2 text-xs sm:text-sm w-full items-center hover:bg-violet-300 hover:text-gray-700 font-semibold rounded-[10px] py-2 text-center ${pathname === '/all-users' && 'bg-violet-300 text-gray-800'} `}>
+          <BsPeopleFill />
+            <NavLink className='' to="/all-users">People</NavLink>
+          </li>
+          <li className={`flex flex-col gap-2 text-xs sm:text-sm w-full items-center hover:bg-violet-300 hover:text-gray-700 font-semibold rounded-[10px] py-2 text-center ${pathname === '/saved' && 'bg-violet-300 text-gray-800'} `}>
+          <BsSaveFill />
+            <NavLink className='' to="/saved">Saved</NavLink>
+          </li>
+        </ul>
+    </div>
   )
 }
 
