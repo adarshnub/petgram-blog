fa1ee85a31d4a62ff6184e88c6b5bbc95201c227
V Adarsh <adarshmanjady@gmail.com>
2023-12-16 18:15:03 +0530

    added bottom nav for sm-devices

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ import SignupForm from "./_auth/forms/SignupForm";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 import {  Toaster } from "@/components/ui/toaster";
+import CreatePost from "./_root/pages/CreatePost";
+import AllUsers from "./_root/pages/AllUsers";
+import Saved from "./_root/pages/Saved";
+import Explore from "./_root/pages/Explore";
 
 const App = () => {
   return (
@@ -21,6 +25,10 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/explore' element={<Explore />} />
+          <Route path='/all-users' element={<AllUsers />} />
+          <Route path='/saved' element={<Saved />} />
         </Route>
       </Routes>
 
