638eb2e1249955a9e324b3d1af3fb5eafd0fcade
V Adarsh <adarshmanjady@gmail.com>
2023-12-30 17:17:47 +0530

    added postTime/comment-ui

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -2,9 +2,13 @@
 import { useUserContext } from "@/context/AuthContext";
 import { Models } from "appwrite";
 import { BiSolidEdit } from "react-icons/bi";
+import { BsFillSendFill } from "react-icons/bs";
 
 import { Link } from "react-router-dom";
 import PostStats from "./PostStats";
+
+import { formatDistanceToNow } from 'date-fns';
+import { parseISO } from 'date-fns';
 
 type PostCardProps = {
   post: Models.Document;
@@ -12,12 +16,15 @@ type PostCardProps = {
 
 const PostCard = ({ post }: PostCardProps) => {
   const {user} = useUserContext();
+  // console.log(user, 'user Data')
+
+  
 
   if(!post.creator) return;
 
   return (
     <div className="bg-gray-800  shadow-2xl rounded-lg py-4 max-w-[30rem] md:h-[38rem]">
-      <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
         
             {/* <div className="absolute top-0 w-full  bg-red-400"></div> */}
             <div className="flex justify-between px-2">
@@ -45,18 +52,48 @@ const PostCard = ({ post }: PostCardProps) => {
           <div>
             <img src={post.imageUrl} 
             className="w-[40rem] max-h-[25rem]"/>
-            <div className="flex w-full px-2 pt-2">
-            <h3 className="text-gray-300 text-sm font-semibold">{post?.creator?.name}</h3> 
-            <p className="text-start text-sm font-light px-2">{post?.caption}</p>
-            </div>
+           
           </div>
-          <div className="flex justify-between md:px-7  text-sm">
+          <div className="flex justify-between md:px-7 pt-2 text-sm">
         
         <PostStats post={post} userId={user.id} />
           </div>
+          <div className="flex w-full px-2 ">
+            <h3 className="text-gray-300 text-sm font-semibold">{post?.creator?.username}</h3> 
+            <p className="text-start text-sm font-light px-2">{post?.caption}</p>
+            </div>
+            <div className="text-gray-400 text-sm text-start pl-2 ">
+              <p className="text-[13px] font-normal">View all comments</p>
+              <div className="flex gap-4 pt-1">
+                <img 
+                src={user.imageUrl}
+                alt="current user"
+                className="rounded-full w-8" />
+                <input
+                placeholder="Add a comment..."
+                className="bg-gray-800 text-[13px] border rounded-full px-4" />
+                <button><BsFillSendFill /></button>
+              </div>
+              <p className="pt-2">{convertCreatedAtToRelativeTime(post?.$createdAt)}</p>
+            </div>
       </div>
     </div>
   );
 };
 
 export default PostCard;
+
+
+
+
+
+export const convertCreatedAtToRelativeTime = (createdAt: string): string => {
+  const parsedDate = parseISO(createdAt);
+  return formatDistanceToNow(parsedDate, { addSuffix: true });
+};
+
+
+// const createdAt = '2023-12-22T11:03:49.768+00:00';
+// const relativeTime = convertCreatedAtToRelativeTime(createdAt);
+
+// console.log(relativeTime,'time');
--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,75 +1,69 @@
 import { useLikePost } from "@/lib/react-query/queriesAndMutations";
-import {  Models } from "appwrite";
+import { Models } from "appwrite";
 import { useState } from "react";
 import { FcLike, FcLikePlaceholder } from "react-icons/fc";
 import { FaRegCommentAlt } from "react-icons/fa";
 import { BsSaveFill, BsSave } from "react-icons/bs";
 import Heart from "react-animated-heart";
 
-
 type PostStatProps = {
-    post: Models.Document,
-    userId: string
-}
+  post: Models.Document;
+  userId: string;
+};
 
-export const checkIsLiked = (likeList:string[],userId:string) => {
-    return likeList.includes(userId)
-}
+export const checkIsLiked = (likeList: string[], userId: string) => {
+  return likeList.includes(userId);
+};
 
-const PostStats = ({post,userId}:PostStatProps) => {
+const PostStats = ({ post, userId }: PostStatProps) => {
+  const likesList = post.likes.map((user: Models.Document) => user.$id);
+  const [likes, setLikes] = useState<string[]>(likesList);
+  const [isliked, setIsLiked] = useState(false);
 
-    const likesList = post.likes.map((user: Models.Document) => user.$id);
-    const [likes,setLikes] = useState<string[]>(likesList);
-    const [isliked, setIsLiked] = useState(false);
+  const { mutate: likePost } = useLikePost();
 
-    const { mutate:likePost} = useLikePost();
+  const handleLikePost = () => {
+    let likesArray = [...likes];
 
-    const handleLikePost = () => {
-        let likesArray = [...likes];
-
-        if(likesArray.includes(userId)) {
-            likesArray = likesArray.filter((Id) => Id !== userId);
-        }else {
-            likesArray.push(userId);
-        }
-
-       if( checkIsLiked(likes,userId)){
-        setIsLiked(false)
-        
-       }
-        setLikes(likesArray);
-        likePost({postId:post.$id,likesArray})
-        console.log(likesArray,'like array')
+    if (likesArray.includes(userId)) {
+      likesArray = likesArray.filter((Id) => Id !== userId);
+    } else {
+      likesArray.push(userId);
     }
 
-    return (
-    
-          <div className="relative flex justify-between px-7 w-full max-w-[30rem] text-sm">
-             <div className=" absolute bottom-7 left-0">
-               <Heart isClick={isliked} onClick={() => { setIsLiked(!isliked);handleLikePost(); }}  />
-               </div>
-            <div className=" flex gap-4 items-center pl-4">
-          
-              <button onClick={handleLikePost}>
-                {
-                   checkIsLiked(likes,userId) ?  <FcLike /> : <FcLikePlaceholder />
-                }
-           
-              </button>
-             {/* <div className=" absolute bottom-[] left-0">
-               <Heart isClick={isliked} onClick={() => { setIsLiked(!isliked);handleLikePost(); }}  />
-               </div> */}
-              <p>{ likes.length > 1 && likes.length } likes </p>
-            </div>
-            
-            
-            <div><FaRegCommentAlt /> </div>
-            <div>
-             { <BsSaveFill /> ||  <BsSave />  }
-            </div>
-          </div>
-    
-  )
-}
+    if (checkIsLiked(likes, userId)) {
+      setIsLiked(false);
+    }
+    setLikes(likesArray);
+    likePost({ postId: post.$id, likesArray });
+    console.log(likesArray, "like array");
+  };
+
+  return (
+    <div className="relative flex justify-between px-7 w-full max-w-[30rem] text-sm">
+      <div className=" absolute bottom-7 left-0">
+        <Heart
+          isClick={isliked}
+          onClick={() => {
+            setIsLiked(!isliked);
+            handleLikePost();
+          }}
+        />
+      </div>
+      <div className=" flex gap-3 items-center pl-4">
+        <button onClick={handleLikePost}>
+          {checkIsLiked(likes, userId) ? <FcLike /> : <FcLikePlaceholder />}
+        </button>
+
+        <p>{likes.length > 1 && likes.length} </p>
+      </div>
+
+      <div>
+        <FaRegCommentAlt />{" "}
+      </div>
+      <div>{<BsSaveFill /> || <BsSave />}</div>
+    </div>
+  );
+};
 
 export default PostStats;
